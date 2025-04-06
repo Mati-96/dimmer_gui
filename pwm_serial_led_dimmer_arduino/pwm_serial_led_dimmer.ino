5780e063b687629ce3aae42ea83c01c63a4f79e2
Matías A. Franks <161748457+Mati-96@users.noreply.github.com>
2025-04-06 16:52:29 -0300

    Update pwm_serial_led_dimmer.ino
    Added English annotations.

--- pwm_serial_led_dimmer_arduino/pwm_serial_led_dimmer.ino
+++ pwm_serial_led_dimmer_arduino/pwm_serial_led_dimmer.ino
@@ -1,20 +1,20 @@
 const int pin2 = 2; //GPIO 2
-const int frec = 5000; //frecuencia de PWM
-const int res = 8; //resolución de 8 bits, valores de 0 a 255
+const int frec = 5000; //PWM frequency
+const int res = 8; //8 bit resolution, values between 0 and 255
 
 void setup() {
   ledcAttach(pin2, frec, res);
-  Serial.begin(9600); //inicia la comunicación serial a 9600 bits por seg (baud)
+  Serial.begin(9600); //initiate serial communication at 9600 bits per second (baud)
 }
 
 void loop() {
-  if (Serial.available() > 0) //verifica la disponibilidad de comunicación serial
+  if (Serial.available() > 0) //verify availability of serial communication
   {    
-    int ciclo = Serial.parseInt(); //lee el valor enviado desde Qt (número entre 0 y 255)
+    int ciclo = Serial.parseInt(); //read a the value sent from the console (between 0 and 255)
 
-    if (ciclo >= 0 && ciclo <= 255) //verifica que el valor esté entre 0 y 255
+    if (ciclo >= 0 && ciclo <= 255) //verify that the value is between 0 and 255
     {
-      analogWrite(pin2, ciclo); //ajusta el ciclo de trabajo (brillo) del LED
+      analogWrite(pin2, ciclo); //adjust LED brightness (cycle)
     }
   }
 }
